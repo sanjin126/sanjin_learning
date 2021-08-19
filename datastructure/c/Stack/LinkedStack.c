3125ea1a35d77544375d1f8b719dcce57e5bd1c3
sanjin <1032628545@qq.com>
2021-08-19 11:12:30 +0800

    LinkedStack & shell

---
+++ datastructure/c/Stack/LinkedStack.c
@@ -0,0 +1,80 @@
+/* 
+ * Target:This is a file contain the defination of Linkedstack for individual learning.
+ * 
+ * Date:2021/8/18
+ * 
+ * Author:sanjin
+ * 
+ * Tips:this linkedstack contains a HEAD node. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define OVERFLOW -2
+typedef int ElemType;
+typedef struct SNode{
+    ElemType data;
+    struct SNode *next;
+} SNode, *Stack; //一个节点类型， 一个指向栈的指针
+
+/* 建立一个带头节点的链栈 */
+Stack InitStack(Stack S)
+{
+    S = malloc(sizeof(SNode)); //不需要强制类型转换， c编译器会进行隐式转换，但c++需要
+
+    if(!S)
+        exit(OVERFLOW);
+    S->next = NULL; //置为空栈
+
+    return S;
+}
+
+bool IsEmpty(Stack S)
+{
+    return S->next == NULL;
+}
+
+bool ClearStack(Stack S)
+{
+    SNode *p, *q;
+
+    if (IsEmpty(S))
+        return false;
+    p = S->next;
+    while (p != NULL)
+    {
+        q = p->next;
+        free(p);
+        p = q;
+    }
+    S->next = NULL; //!!!在释放所有元素后，最后要将栈置空
+
+    return true;
+}
+
+bool Push(Stack S, ElemType e)
+{
+    SNode *TmpCell = malloc(sizeof(SNode));
+
+    TmpCell->data = e;
+    TmpCell->next = S->next;
+    S->next = TmpCell;
+    return true;
+}
+
+bool Pop(Stack S, ElemType *PtrToData)
+{
+    SNode *TmpCell;
+
+    if (IsEmpty(S))
+        return false;
+    *PtrToData = S->next->data;
+    TmpCell = S->next;
+    S->next = S->next->next;
+    free(TmpCell);
+
+    return true;
+}
---
+++ shell/screen.sh
@@ -0,0 +1,7 @@
+#/usr/bin/bash
+
+xrandr --newmode Modeline "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+
+xrandr --addmode VGA-1 "1920x1080_60.00"
+
+xrandr --output VGA-1 --mode "1920x1080_60.00"
